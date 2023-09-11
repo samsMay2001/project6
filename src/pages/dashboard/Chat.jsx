c7e7f17ffc5816a8db6009dfc8babf424a17ca0b
sam <samscloud2@gmail.com>
2023-09-11 10:23:36 -0300

    added the archive text and box

--- src/pages/dashboard/Chat.jsx
+++ src/pages/dashboard/Chat.jsx
@@ -1,40 +1,55 @@
-import { Box, Stack, Typography, InputBase} from "@mui/material";
+import { Box, Stack, Typography, InputBase, Button, Divider } from "@mui/material";
 // import InputBase from "@mui/material/InputBase/InputBase";
 
-import {styled, alpha} from '@mui/material/styles'
-import { CircleDashed, MagnifyingGlass } from "phosphor-react";
+import { styled, alpha } from '@mui/material/styles'
+import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
 
 
-const Search = styled("div")(({theme})=>(
+const ChatElement = () => {
+    return (
+        <Box sx={{
+            width: '100%', 
+            height: 60, 
+            borderRadius: 1, 
+            background: '#fff'
+        }}
+        p={2}
+        >
+
+        </Box>
+    )
+}
+
+const Search = styled("div")(({ theme }) => (
     {
-        position : 'relative', 
-        borderRadius : 20,
+        position: 'relative',
+        borderRadius: 20,
         // border : '1px dashed grey',
-        backgroundColor : alpha(theme.palette.background.paper,1), 
-        marginRight: theme.spacing(2), 
+        backgroundColor: alpha(theme.palette.background.paper, 1),
+        marginRight: theme.spacing(2),
         marginLeft: 0,
         width: '100%',
         // height: '50px'
     }
 ))
-const SearchIconWrapper = styled("div")(({theme})=>(
+const SearchIconWrapper = styled("div")(({ theme }) => (
     {
-        padding: theme.spacing(0, 2), 
+        padding: theme.spacing(0, 2),
         // border: '1px dashed grey',
         height: "100%",
-        position: 'absolute', 
-        pointerEvents: 'none', 
-        display: 'flex', 
-        alignItems : 'center', 
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
         justifyContent: 'center'
     }
 ))
-const StyledInputBase = styled(InputBase)(({theme})=>(
+const StyledInputBase = styled(InputBase)(({ theme }) => (
     {
-        color : "inherit", 
+        color: "inherit",
         "& .MuiInputBase-input": {
-            padding: theme.spacing(1, 1, 1, 0), 
-            paddingLeft : `calc(1em + ${theme.spacing(4)})`, 
+            padding: theme.spacing(1, 1, 1, 0),
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
             width: '100%'
         }
     }
@@ -51,21 +66,31 @@ function Chats() {
             boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)'
         }}>
             <Stack p={3} spacing={2}>
-                <Stack direction="row" alignItems={'center' } justifyContent={'space-between'}>
+                <Stack direction="row" alignItems={'center'} justifyContent={'space-between'}>
                     <Typography variant={'h5'}>
                         Chats
                     </Typography>
-                    <CircleDashed/>
+                    <CircleDashed />
                 </Stack>
 
                 {/* search bar */}
-                <Stack sx={{width: '100%' }}>
+                <Stack sx={{ width: '100%' }} spacing={1}>
                     <Search>
                         <SearchIconWrapper>
-                            <MagnifyingGlass color="#709CE6"/>
+                            <MagnifyingGlass color="#709CE6" />
                         </SearchIconWrapper>
-                        <StyledInputBase placeholder = {'Search...'} inputProps={{'aria-label': 'search'}} />
+                        <StyledInputBase placeholder={'Search...'} inputProps={{ 'aria-label': 'search' }} />
                     </Search>
+                    <Stack spacing={1}>
+                        <Stack direction={'row'} alignItems={'center'} spacing={1.5}>
+                            <ArchiveBox size={24} />
+                            <Button>Archive</Button>
+                        </Stack>
+                        <Divider />
+                    </Stack>
+                    <Stack direction={'column'}>
+                        <ChatElement/>
+                    </Stack>
                 </Stack>
             </Stack>
         </Box>
