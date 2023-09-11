c10c170bec0711bf327566d2a6c3f562c9ffa4ab
sam <samscloud2@gmail.com>
2023-09-11 11:12:35 -0300

    pinned chats list

--- src/pages/dashboard/Chat.jsx
+++ src/pages/dashboard/Chat.jsx
@@ -5,37 +5,38 @@ import { Box, Stack, Typography, InputBase, Button, Divider, Badge, Avatar } fro
 import { styled, alpha } from '@mui/material/styles'
 import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
 import { useEffect } from "react";
+import { ChatList } from "../../data";
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
     '& .MuiBadge-badge': {
-      backgroundColor: '#44b700',
-      color: '#44b700',
-      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-      '&::after': {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        borderRadius: '50%',
-        animation: 'ripple 1.2s infinite ease-in-out',
-        border: '1px solid currentColor',
-        content: '""',
-      },
+        backgroundColor: '#44b700',
+        color: '#44b700',
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        '&::after': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            borderRadius: '50%',
+            animation: 'ripple 1.2s infinite ease-in-out',
+            border: '1px solid currentColor',
+            content: '""',
+        },
     },
     '@keyframes ripple': {
-      '0%': {
-        transform: 'scale(.8)',
-        opacity: 1,
-      },
-      '100%': {
-        transform: 'scale(2.4)',
-        opacity: 0,
-      },
+        '0%': {
+            transform: 'scale(.8)',
+            opacity: 1,
+        },
+        '100%': {
+            transform: 'scale(2.4)',
+            opacity: 0,
+        },
     },
-  }));
+}));
 
-const ChatElement = () => {
+const ChatElement = ({ id, name, img, msg, time, unread, online }) => {
     // const [avatarImg, ]
     // useEffect(()=>{
 
@@ -49,28 +50,36 @@ const ChatElement = () => {
             p={2}
         >
             <Stack direction="row" alignItems='center' justifyContent={'space-between'}>
+                {/* avatar section */}
                 <Stack direction={'row'} spacing={2}>
-                    <StyledBadge 
-                        overlap="circular"
-                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                        variant="dot">
+                    {online ?
+
+                        <StyledBadge
+                            overlap="circular"
+                            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                            variant="dot">
+                            <Avatar src={faker.image.avatar()} />
+                        </StyledBadge> :
                         <Avatar src={faker.image.avatar()} />
-                    </StyledBadge>
+
+                    }
+                    {/* text section */}
                     <Stack spacing={0.3}>
                         <Typography variant={'subtitle2'}>
-                            Pedro Alejandro
+                            {name}
                         </Typography>
                         <Typography variant={'caption'}>
-                            How are you?
+                            {msg}
                         </Typography>
                     </Stack>
-                    <Stack spacing={2} alignItems='center' >
-                        <Typography sx={{fontWeigth: 600}} variant="caption">9:36</Typography>
-                        <Badge color="primary" badgeContent={2}/>
-                    </Stack>
+                </Stack>
+                {/* time section */}
+                <Stack spacing={2} alignItems='center' >
+                    <Typography sx={{ fontWeigth: 600 }} variant="caption">{time}</Typography>
+                    <Badge color="primary" badgeContent={unread} />
                 </Stack>
             </Stack>
-                
+
         </Box>
     )
 }
@@ -144,7 +153,12 @@ function Chats() {
                         <Divider />
                     </Stack>
                     <Stack direction={'column'}>
-                        <ChatElement />
+                        <Stack spacing={2.4}>
+                            <Typography variant={'subtitle2'} sx={{ color: '#676767' }}>Pinned</Typography>
+                            {ChatList.filter((el) => el.pinned).map((el) => (
+                                <ChatElement {...el} />
+                            ))}
+                        </Stack>
                     </Stack>
                 </Stack>
             </Stack>
