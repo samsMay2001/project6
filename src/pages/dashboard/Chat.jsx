8770242c4a7a09656798121891f3433a86058f14
sam <samscloud2@gmail.com>
2023-09-11 10:55:37 -0300

    chat element

--- src/pages/dashboard/Chat.jsx
+++ src/pages/dashboard/Chat.jsx
@@ -1,21 +1,76 @@
-import { Box, Stack, Typography, InputBase, Button, Divider } from "@mui/material";
+import { faker } from "@faker-js/faker";
+import { Box, Stack, Typography, InputBase, Button, Divider, Badge, Avatar } from "@mui/material";
 // import InputBase from "@mui/material/InputBase/InputBase";
 
 import { styled, alpha } from '@mui/material/styles'
 import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
+import { useEffect } from "react";
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      backgroundColor: '#44b700',
+      color: '#44b700',
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      '&::after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        borderRadius: '50%',
+        animation: 'ripple 1.2s infinite ease-in-out',
+        border: '1px solid currentColor',
+        content: '""',
+      },
+    },
+    '@keyframes ripple': {
+      '0%': {
+        transform: 'scale(.8)',
+        opacity: 1,
+      },
+      '100%': {
+        transform: 'scale(2.4)',
+        opacity: 0,
+      },
+    },
+  }));
 
 const ChatElement = () => {
+    // const [avatarImg, ]
+    // useEffect(()=>{
+
+    // }, [])
     return (
         <Box sx={{
-            width: '100%', 
-            height: 60, 
-            borderRadius: 1, 
+            width: '100%',
+            borderRadius: 1,
             background: '#fff'
         }}
-        p={2}
+            p={2}
         >
-
+            <Stack direction="row" alignItems='center' justifyContent={'space-between'}>
+                <Stack direction={'row'} spacing={2}>
+                    <StyledBadge 
+                        overlap="circular"
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                        variant="dot">
+                        <Avatar src={faker.image.avatar()} />
+                    </StyledBadge>
+                    <Stack spacing={0.3}>
+                        <Typography variant={'subtitle2'}>
+                            Pedro Alejandro
+                        </Typography>
+                        <Typography variant={'caption'}>
+                            How are you?
+                        </Typography>
+                    </Stack>
+                    <Stack spacing={2} alignItems='center' >
+                        <Typography sx={{fontWeigth: 600}} variant="caption">9:36</Typography>
+                        <Badge color="primary" badgeContent={2}/>
+                    </Stack>
+                </Stack>
+            </Stack>
+                
         </Box>
     )
 }
@@ -89,7 +144,7 @@ function Chats() {
                         <Divider />
                     </Stack>
                     <Stack direction={'column'}>
-                        <ChatElement/>
+                        <ChatElement />
                     </Stack>
                 </Stack>
             </Stack>
