e27c85449d22b50302a401703a65148bf0c0f250
sam <samscloud2@gmail.com>
2023-10-19 12:44:36 -0300

    login route, component, logo, and page

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,12 +1,20 @@
+import { Container, Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
-
+import logo from '../../assets/Images/logo.ico'
 const MainLayout = () => {
   return (
     <>
-      <div>Main Layout</div>
+      <Container sx={{mt: 5}} maxWidth="sm">
+        <Stack spacing={5}>
+          <Stack  sx={{width: '100%'}} direction={'column'} alignItems={'center'}>
+            <img style={{height:120, width: 120}} src={logo} alt="Logo"/>
+          </Stack>
+        </Stack>
+        <div></div>
+        <Outlet />
+      </Container>
 
-      <Outlet />
     </>
   );
 };
