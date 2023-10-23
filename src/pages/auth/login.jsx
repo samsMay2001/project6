39cdf73ebd7c63ba23003bba044cec929fa0c7a8
sam <samscloud2@gmail.com>
2023-10-23 15:37:03 -0300

    added Login icons, starting to work on the login form

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,6 +1,25 @@
+import { Link, Stack, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import AuthSocial from "./AuthSocial";
+
 function Login() {
     return ( 
-        <div>Login</div>
+        <Stack spacing={2} sx={{mb: 5, position: "relative"}}>
+            <Typography variant="h4">
+                Login to Tawk
+            </Typography>
+            <Stack direction={'row'} spacing={0.5}>
+                <Typography variant="body2">New User?</Typography>
+                <Link to={'/auth/register'} component={RouterLink} variant={'subtitle2'}>
+                    Create Account
+                </Link> 
+            </Stack>
+
+            {/* Login Form  */}
+            
+            {/* AuthSocial */}
+            <AuthSocial/>
+        </Stack>
      );
 }
 
