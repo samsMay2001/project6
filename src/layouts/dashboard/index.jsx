a1e0e7aa44a4987a74cf03d8fa37b7b3fca33c9f
samsMay2001 <muhigirwasamuel2001@gmail.com>
2023-11-11 16:11:56 -0400

    auth navigation

--- src/layouts/dashboard/index.jsx
+++ src/layouts/dashboard/index.jsx
@@ -1,13 +1,13 @@
 import { Stack } from "@mui/material";
 import { useTheme, styled } from "@mui/material/styles";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useState } from "react";
 import Switch from "@mui/material/Switch/Switch";
 import useSettings from "../../hooks/useSettings";
 import SideBar from "./sidebar";
 
-
+const isAuthenticated = true; 
 const DashboardLayout = () => {
 
   const theme = useTheme()
@@ -15,6 +15,11 @@ const DashboardLayout = () => {
   const [selected, setSelected] = useState(0)
 
   const { onToggleMode } = useSettings();
+  if (!isAuthenticated){
+    return (
+      <Navigate to={'/auth/login'} />
+    )
+  }
 
   return (
     <Stack direction={'row'} sx={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0}}>
--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,9 +1,14 @@
 import { Container, Stack } from "@mui/material";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import logo from '../../assets/Images/logo.ico'
 import { Columns } from "phosphor-react";
+
+const isAuthenticated = true;
 const MainLayout = () => {
+  if (isAuthenticated) {
+    return <Navigate to={'/app'} />
+  }
   return (
     <>
       <Stack direction={'column'} justifyContent={'center'} alignItems={'center'} sx={{ height: '100vh'}}>
