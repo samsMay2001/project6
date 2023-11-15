7ee56db62feece3c6154a65f9cb5303770818b4a
Samuel Muhigirwa <127790767+samsMay2001@users.noreply.github.com>
2023-11-15 21:05:52 +0000

    set LoginUser to return a valid return value

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,30 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    isLoggedIn : false, 
-    token: '', 
-    isLoading: false,
-}
+  isLoggedIn: false,
+  token: "",
+  isLoading: false,
+};
 
 const slice = createSlice({
-    name: 'auth', 
-    initialState, 
-    reducers: {
-        logIn(state, action) {
-            state.isLoggedIn = action.payload.isLoggedIn; 
-            state.token = action.payload.token;
-        }, 
-        signOut(state, action) {
-            state.isLoggedIn = false; 
-            state.token = ''; 
-        }
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    logIn(state, action) {
+      state.isLoggedIn = action.payload.isLoggedIn;
+      state.token = action.payload.token;
+    },
+    signOut(state, action) {
+      state.isLoggedIn = false;
+      state.token = "";
+    },
+  },
+});
 
-export default slice.reducer; 
+export default slice.reducer;
 
 // login
 
-export function LoginUser(formValues){
-    console.log(formValues); 
+export function LoginUser(formValues) {
+  return async () => {
+    console.log(formValues);
+  };
 }
