c1f572c4223e211924981da8513e7e33808dd5b5
samsMay2001 <muhigirwasamuel2001@gmail.com>
2023-11-15 15:16:15 -0400

    auth state management

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,1 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isLoggedIn : false, 
+    token: '', 
+    isLoading: false,
+}
+
+const slice = createSlice({
+    name: 'auth', 
+    initialState, 
+    reducers: {
+        logIn(state, action) {
+            state.isLoggedIn = action.payload.isLoggedIn; 
+            state.token = action.payload.token;
+        }, 
+        signOut(state, action) {
+            state.isLoggedIn = false; 
+            state.token = ''; 
+        }
+    }
+})
+
+export default slice.reducer; 
+
+// login
+
+export function LoginUser(formValues){
+    
+}
