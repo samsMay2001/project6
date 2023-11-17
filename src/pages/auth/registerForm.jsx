06ed9a8050e7160a85b3b53269b6864eff852248
Samuel Muhigirwa <127790767+samsMay2001@users.noreply.github.com>
2023-11-17 15:42:33 +0000

    user creation feature

--- src/pages/auth/registerForm.jsx
+++ src/pages/auth/registerForm.jsx
@@ -1,93 +1,133 @@
-import { useState } from "react";
-import * as Yup from 'yup'
-import {useForm} from 'react-hook-form'
-import {yupResolver} from "@hookform/resolvers/yup"
+import { useEffect, useState } from "react";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { async } from "emoji-mart";
 import FormProvider from "../../components/hook-form/FormProvider";
-import { Alert, Button, IconButton, InputAdornment, Stack } from "@mui/material";
+import {
+  Alert,
+  Button,
+  IconButton,
+  InputAdornment,
+  Stack,
+} from "@mui/material";
 import { RHFTextField } from "../../components/hook-form";
 import { Eye, EyeSlash } from "phosphor-react";
 import { Link } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import {
+  createUser,
+  LoginUser,
+  resetErrorMessage,
+} from "../../redux/slices/auth";
+import { useSelector } from "react-redux";
 
 function RegisterForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const { errorMessage } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const RegisterSchema = Yup.object().shape({
+    firstname: Yup.string().required("First name is required"),
+    lastname: Yup.string().required("Last name is required"),
+    email: Yup.string().required("Email is required").email("Email must valid"),
+    password: Yup.string().required("Password is required"),
+  });
 
-    const [showPassword, setShowPassword] = useState(false); 
+  const defaultValues = {
+    firstname: "",
+    lastname: "",
+    email: "demo@tawk.com",
+    password: "demo@1234",
+  };
 
-    const RegisterSchema = Yup.object().shape({
-        firstname: Yup.string().required('First name is required'),
-        lastname: Yup.string().required('Last name is required'),
-        email: Yup.string().required('Email is required').email('Email must valid'), 
-        password: Yup.string().required('Password is required')
-    })
+  const methods = useForm({
+    resolver: yupResolver(RegisterSchema),
+    defaultValues,
+  });
 
-    const defaultValues = {
-        firstname: '', 
-        lastname: '',
-        email: 'demo@tawk.com', 
-        password: 'demo@1234'
+  const {
+    reset,
+    setError,
+    handleSubmit,
+    formState: { errors, isSubmitted, isSubmitSuccessful },
+  } = methods;
+
+  const onSubmit = async (data) => {
+    try {
+      // submit data
+      //   console.log(data);
+      dispatch(createUser(data));
+    } catch (err) {
+      console.log(err);
+      reset();
+      setError("afterSubmit", {
+        ...err,
+        message: err.message,
+      });
     }
+  };
 
-    const methods = useForm({
-        resolver: yupResolver(RegisterSchema), 
-        defaultValues
-    })
-
-    const {
-        reset, 
-        setError, 
-        handleSubmit, 
-        formState : {errors, isSubmitted, isSubmitSuccessful}
-    } = methods;
-    
-    const onSubmit = async (data) => {
-        try{
-            // submit data
-        }catch(err){
-            console.log(err); 
-            reset(); 
-            setError("afterSubmit", {
-                ...err, 
-                message: err.message
-            })
-        }
+  useEffect(() => {
+    // displays errors that may have occured while loging in on the server.
+    if (errorMessage && errorMessage.length > 1) {
+      reset();
+      setError("afterSubmit", {
+        message: errorMessage,
+      });
+      dispatch(resetErrorMessage());
     }
-    return ( 
-        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-            <Stack spacing={3}>
-                {!!errors.afterSubmit && <Alert severity="error">{errors.afterSubmit.message}</Alert>}
-                <Stack direction={{xs: 'column', sm:'row'}} spacing={2}>
-                    <RHFTextField name={'firstname'} label={"First name"} />
-                    <RHFTextField name={'lastname'} label={"Last name"} />
-                </Stack>
-                <RHFTextField name={'email'} label={"Email address"} />
-                <RHFTextField name={'password'} label={"Password"} type={showPassword ? "text" : "password"} 
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment>
-                                <IconButton onClick={()=> {
-                                    setShowPassword(!showPassword)
-                                }}>
-                                    {showPassword ? <Eye/> : <EyeSlash/>}
-                                </IconButton>
-                            </InputAdornment>
-                        )
-                    }} 
-                />
-                <Button fullWidth color="inherit" size="large" type="submit" variant="contained" sx={{
-                    bgcolor: 'text.primary', 
-                    color: (theme) => theme.palette.mode === "light" ? "common.white" : "grey.800", 
-                    "&:hover": {
-                        bgcolor: "text.primary", 
-                        color: (theme) => 
-                        theme.palette.mode === "light" ? "common.white" : "grey.800"
-                    }
-                }}>
-                    Register
-                </Button>
-            </Stack>
-        </FormProvider>
-     );
+  }, [errorMessage]);
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        {!!errors.afterSubmit && (
+          <Alert severity="error">{errors.afterSubmit.message}</Alert>
+        )}
+        <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+          <RHFTextField name={"firstname"} label={"First name"} />
+          <RHFTextField name={"lastname"} label={"Last name"} />
+        </Stack>
+        <RHFTextField name={"email"} label={"Email address"} />
+        <RHFTextField
+          name={"password"}
+          label={"Password"}
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment>
+                <IconButton
+                  onClick={() => {
+                    setShowPassword(!showPassword);
+                  }}
+                >
+                  {showPassword ? <Eye /> : <EyeSlash />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button
+          fullWidth
+          color="inherit"
+          size="large"
+          type="submit"
+          variant="contained"
+          sx={{
+            bgcolor: "text.primary",
+            color: (theme) =>
+              theme.palette.mode === "light" ? "common.white" : "grey.800",
+            "&:hover": {
+              bgcolor: "text.primary",
+              color: (theme) =>
+                theme.palette.mode === "light" ? "common.white" : "grey.800",
+            },
+          }}
+        >
+          Register
+        </Button>
+      </Stack>
+    </FormProvider>
+  );
 }
 
 export default RegisterForm;
--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,13 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
-import axios from '../../utils/axios';
+import axios from "../../utils/axios";
 import { dispatch } from "../store";
-
 
 const initialState = {
   isLoggedIn: false,
   token: "",
   isLoading: false,
-  errorMessage: ""
+  errorMessage: "",
 };
 
 const slice = createSlice({
@@ -17,16 +16,16 @@ const slice = createSlice({
     logIn(state, action) {
       state.isLoggedIn = action.payload.isLoggedIn;
       state.token = action.payload.token;
-      state.errorMessage  = action.payload.errorMessage
+      state.errorMessage = action.payload.errorMessage;
     },
     signOut(state, action) {
       state.isLoggedIn = false;
       state.token = "";
-      state.errorMessage = ""; 
+      state.errorMessage = "";
     },
-    resetErrorMessage(state, action){
-      state.errorMessage = ""
-    }
+    resetErrorMessage(state, action) {
+      state.errorMessage = "";
+    },
   },
 });
 
@@ -36,32 +35,74 @@ export default slice.reducer;
 
 export function LoginUser(formValues) {
   return async () => {
-    await axios.post("/login", {...formValues}, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }).then((res)=> {
-      dispatch(slice.actions.logIn({
-        isLoggedIn: true, 
-        token: res.data.token
-      }))
-    }).catch((err)=> {
-      dispatch(slice.actions.logIn({
-        errorMessage : err.response.data.message
-      }))
-      // console.log(err.response.data.message)
-    })
+    await axios
+      .post(
+        "/login",
+        { ...formValues },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      )
+      .then((res) => {
+        dispatch(
+          slice.actions.logIn({
+            isLoggedIn: true,
+            token: res.data.token,
+          }),
+        );
+      })
+      .catch((err) => {
+        dispatch(
+          slice.actions.logIn({
+            errorMessage: err.response.data.message,
+          }),
+        );
+        // console.log(err.response.data.message)
+      });
   };
 }
 
-export function logoutUser(){
+export function logoutUser() {
   return async () => {
-    dispatch(slice.actions.signOut())
-  }
+    dispatch(slice.actions.signOut());
+  };
 }
 
-export function resetErrorMessage(){
-  return async ()=> {
-    dispatch(slice.actions.resetErrorMessage()); 
-  }
+export function resetErrorMessage() {
+  return async () => {
+    dispatch(slice.actions.resetErrorMessage());
+  };
+}
+
+export function createUser(formValues) {
+  return async () => {
+    await axios
+      .post(
+        "/newuser",
+        { ...formValues },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      )
+      .then((res) => {
+        dispatch(
+          slice.actions.logIn({
+            isLoggedIn: true,
+            token: res.data.token,
+          }),
+        );
+      })
+      .catch((err) => {
+        dispatch(
+          slice.actions.logIn({
+            errorMessage: err.response.data.error,
+          }),
+        );
+        // console.log(err.response.data.message)
+      });
+  };
 }
