09290736696a73b7730311ae811538b8530cfb30
samsMay2001 <muhigirwasamuel2001@gmail.com>
2023-11-19 15:11:40 -0400

    updated layout to show new friends and friend reqs

--- src/pages/dashboard/AddFriends.jsx
+++ src/pages/dashboard/AddFriends.jsx
@@ -1,20 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { FetchFriends, FetchUsers } from "../../redux/slices/app";
 import { dispatch } from "../../redux/store";
+import { Button, Stack, Tab, Tabs, useTheme } from "@mui/material";
 
 export function AddFriends() {
   const {friends, users} = useSelector((state)=> state.app )
   const {_id} = useSelector((state) => state.auth)
-
-
+  const [tab, setTab] = useState(0)
+  const theme = useTheme()
+  function handleChange(event, value){
+    setTab(value)
+  }
   useEffect(() => {
     dispatch(FetchUsers(friends, _id))
   }, []);
   return (
-    (users && users.map((item, index)=> (
-      <div>User {index+1}</div>
-    )))
+    <div>
+      <Stack sx={{width: "100%"}} direction={'row'} spacing={2}>
+        <Button sx={{
+          backgroundColor:  tab === 0 ? (theme.palette.mode ==="light" ? "rgb(0,0,0,.06)" : "#36414E") : "rgb(0, 0, 0, 0)",
+          color: theme.palette.mode ==="light" ? '#000' : "rgba(255, 255, 255, 0.8)",
+          '&:hover': {
+            backgroundColor: theme.palette.mode === "light" ? 'rgb(0,0,0,.06)' : '#333E4A', // Change to the desired hover color
+          }
+        }} onClick={()=> {setTab(0)}}>Explore</Button>
+        <Button sx={{
+          backgroundColor:  tab === 1 ? (theme.palette.mode ==="light" ? "rgb(0,0,0,.06)" : "#36414E") : "rgb(0, 0, 0, 0)",
+          color: theme.palette.mode ==="light" ? '#000' : "rgba(255, 255, 255, 0.8)",
+          '&:hover': {
+            backgroundColor: theme.palette.mode === "light" ? 'rgb(0,0,0,.06)' : '#333E4A', // Change to the desired hover color
+          }
+        }} onClick={()=> {setTab(1)}}>Requests</Button>
+      </Stack>
+      {(users && tab === 0) && users.map((item, index)=> (
+        <div>User {index+1}</div>
+      ))}
+    </div>
   );
 }
 
