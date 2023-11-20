bc858c6e860a52a75b44bacb5cc868616586744a
Samuel Muhigirwa <127790767+samsMay2001@users.noreply.github.com>
2023-11-20 22:56:31 +0000

    Update 4 files

--- src/layouts/dashboard/index.jsx
+++ src/layouts/dashboard/index.jsx
@@ -2,24 +2,55 @@ import { Stack } from "@mui/material";
 import { useTheme, styled } from "@mui/material/styles";
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Switch from "@mui/material/Switch/Switch";
 import useSettings from "../../hooks/useSettings";
 import SideBar from "./sidebar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { connectSocket, socket } from "../../socket";
 
 const isAuthenticated = false;
 const DashboardLayout = () => {
   const theme = useTheme();
-  const {isLoggedIn} = useSelector((state) => state.auth)
-  const [selected, setSelected] = useState(0);
-
+  const {isLoggedIn, user_id} = useSelector((state) => state.auth)
+  const [selected, setSelected] = useState(0); 
+  const dispatch = useDispatch(); 
   const { onToggleMode } = useSettings();
+  
+
+  useEffect(()=>{
+    // friend requests socket code. 
+    // window.onload = () => {
+    //   if (!window.location.hash){
+    //     window.location = window.location + "#loaded"; 
+    //     window.location.reload(); 
+    //   }
+    // }
+    // window.reload();
+    // if (!socket){
+    //   connectSocket(user_id)
+    // }
+
+    // socket.on("new_friend_request", (data)=> {
+    //   console.log('friend request was received')
+    // })
+    // socket.on("request_accepted", (data)=> {
+    //   console.log('friend request was received')
+    // })
+    // socket.on("request_sent", (data)=> {
+    //   console.log('friend request was received')
+    // })
+
+    // return () => {
+    //   socket.off("new_friend_request")
+    //   socket.off("request_accepted")
+    //   socket.off("request_sent")
+    // }
+
+  }, [])
   if (!isLoggedIn) {
-    console.log(isLoggedIn)
     return <Navigate to={"/auth/login"} />;
   }
-
   return (
     <Stack
       direction={"row"}
